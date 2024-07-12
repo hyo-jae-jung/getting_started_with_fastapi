cd248d2f21bae9b016f3b2800761c913b1856263
hyo-jae-jung <hyojaejung@kakao.com>
2024-07-12 15:53:19 +0900

    update test_creature

--- part3/test/unit/service/test_creature.py
+++ part3/test/unit/service/test_creature.py
@@ -3,3 +3,24 @@ import sys
 sys.path.append("/mnt/d/hyojaejung/workspace/Mywork/getting_started_with_fastapi/part3")
 from model.creature import Creature
 from service import creature as code 
+
+sample = Creature(
+    name="Yeti",
+    country="CN",
+    area="Himalayas",
+    description="Hirsute Himalayan",
+    aka="Abominable Snowman"
+)
+
+def test_create():
+    resp = code.create(sample)
+    assert resp == sample 
+
+def test_get_exists():
+    resp = code.get_one("Yeti")
+    assert resp == sample 
+
+def test_get_missing():
+    resp = code.get_one("boxturtle")
+    assert resp in None
+    
